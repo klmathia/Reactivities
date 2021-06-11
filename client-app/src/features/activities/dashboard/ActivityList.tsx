027eadb6f5dad6c7cc20852b88714198fea57d5e
MKL101 <mathias.kleinheinz@wingd.com>
2021-06-11 08:49:28 +0200

    End of section 8

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,5 +1,6 @@
 import { observer } from 'mobx-react-lite';
 import React, { SyntheticEvent, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Item, Segment, Button, Label } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
 
@@ -11,7 +12,7 @@ export default observer(function ActivityList() {
 
     function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
         setTarget(e.currentTarget.name);
-        deleteActivity(id);
+        activityStore.deleteActivity(id);
     }
 
 
@@ -28,13 +29,13 @@ export default observer(function ActivityList() {
                                 <div>{activity.city}, {activity.venue}</div>
                             </Item.Description>
                             <Item.Extra>
-                                <Button onClick={() => activityStore.selectActivity(activity.id)} floated='right' content='View' color='blue' />
+                                <Button as={Link} to={`/activities/${activity.id}`} floated='right' content='View' color='blue' />
                                 <Button 
                                     name={activity.id}
                                     loading={loading && target === activity.id} 
                                     onClick={(e) => handleActivityDelete(e, activity.id)} 
                                     floated='right' 
-                                    content='View' 
+                                    content='Delete' 
                                     color='red' 
                                 />
                                 <Label basic content={activity.description} />
