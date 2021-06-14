cbfda2652eaef2002465ea1542655f78095861c2
MKL101 <mathias.kleinheinz@wingd.com>
2021-06-14 17:43:19 +0200

    End of section 11

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -2,7 +2,8 @@ import React, {SyntheticEvent, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Icon, Item, Segment } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
-import { useStore } from '../../../app/stores/store'; 
+import { useStore } from '../../../app/stores/store';
+import { format } from 'date-fns';
 
 interface Props {
     activity: Activity
@@ -38,7 +39,7 @@ export default function ActivityListItem ({activity}: Props) {
             </Segment>
             <Segment>
                 <span>
-                    <Icon name='clock' /> {activity.date}
+                    <Icon name='clock' /> {format(activity.date!,'dd MMM yyyy h:mm aa')}
                     <Icon name='marker' /> {activity.venue}
                 </span>
             </Segment>
